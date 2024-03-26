dcd8c09e24b4b39a6150481f5ff2af00b75c2618
Renny S <15965840+OfficialRenny@users.noreply.github.com>
2024-03-26 01:47:15 +0000

    A few new things Added a new way to group items, can do a single column of rarities, or split items based on the item type horizontally. Added some config options. Distance between items can be changed via slider, and items are now dropped in the direction+pitch the character is facing, but can be toggled off to just drop items flat (but may cause clipping)

--- loot_teleporter/__init__.py
+++ loot_teleporter/__init__.py
@@ -1,9 +1,9 @@
-from math import cos, sin, sqrt
+from math import sin, cos, sqrt, radians
 from typing import List, Tuple, Union
 
 import unrealsdk #type ignore
 from unrealsdk.unreal import WrappedStruct, UObject
-from mods_base import build_mod, keybind, get_pc, BoolOption
+from mods_base import build_mod, keybind, get_pc, BoolOption, DropdownOption, SliderOption
 
 if True:
     assert __import__("mods_base").__version_info__ >= (1, 0), "Please update the SDK"
@@ -11,10 +11,42 @@ if True:
 __version__: str
 __version_info__: tuple[int, ...]
 
-include_echo_logs = BoolOption("include_echo_logs", False, "Yes", "No", display_name="Include Echo Logs", description="Include Echo Logs?")
+loot_sort_type = DropdownOption(
+    "loot_sort_type",
+    "Rarity",
+    ["Rarity", "Rarity then Type"],
+    display_name="Loot Sort Type",
+    description="Sort loot by rarity or rarity then type"
+)
+
+loot_distance = SliderOption(
+    "loot_distance",
+    75, 50, 200, 25,
+    True,
+    display_name="Loot Distance",
+    description="Distance between sorted loot items"
+)
+
+include_echo_logs = BoolOption(
+    "include_echo_logs",
+    False,
+    "Yes",
+    "No",
+    display_name="Include Echo Logs",
+    description="Include Echo Logs?"
+)
+
+lay_items_flat = BoolOption(
+    "flat_items",
+    False,
+    "Yes",
+    "No",
+    display_name="Lay Items Flat",
+    description="If enabled, items will be laid flat on the ground (items may clip through the floor if facing upward slopes). If disabled, items will be placed in the upward/downwards direction you are facing."
+)
 
 def get_test_library() -> UObject:
-    return unrealsdk.find_object('TestLibrary', '/Script/GbxTest.Default__TestLibrary');
+    return unrealsdk.find_object('TestLibrary', '/Script/GbxTest.Default__TestLibrary')
 
 def get_current_world() -> UObject:
     return get_test_library().GetWorldForActor(get_pc())
@@ -22,27 +54,135 @@ def get_current_world() -> UObject:
 def get_vector(x, y, z) -> WrappedStruct:
     return unrealsdk.make_struct("Vector", X=x, Y=y, Z=z)
 
-# Math stuff from Juso's mod
-u_rotation_180 = 32768
-u_rotation_90 = u_rotation_180 / 2
-u_pi = 3.1415926
-u_conversion = u_pi / u_rotation_180
+def add_vectors(vec1: WrappedStruct, vec2: WrappedStruct) -> WrappedStruct:
+    return get_vector(vec1.X + vec2.X, vec1.Y + vec2.Y, vec1.Z + vec2.Z)
 
-def rot_to_vec3d(rotation: List[int]) -> List[float]:
-    """Takes UE3 Rotation as List, returns List of normalized vector."""
-    f_yaw = rotation[1] * u_conversion
-    f_pitch = rotation[0] * u_conversion
-    cos_pitch = cos(f_pitch)
-    x = cos(f_yaw) * cos_pitch
-    y = sin(f_yaw) * cos_pitch
-    z = sin(f_pitch)
-    return [x, y, z]
+def subtract_vectors(vec1: WrappedStruct, vec2: WrappedStruct) -> WrappedStruct:
+    return get_vector(vec1.X - vec2.X, vec1.Y - vec2.Y, vec1.Z - vec2.Z)
 
-def normalize_vec(
-    vector: Union[List[float], Tuple[float, float, float]]
-) -> List[float]:
-    _len = sqrt(sum(x * x for x in vector))
-    return [x / _len for x in vector]
+def multiply_vector_by_scalar(vec: WrappedStruct, scalar: float) -> WrappedStruct:
+    return get_vector(vec.X * scalar, vec.Y * scalar, vec.Z * scalar)
+
+def divide_vector_by_scalar(vec: WrappedStruct, scalar: float) -> WrappedStruct:
+    if scalar == 0:
+        raise ValueError("Cannot divide by zero")
+    return get_vector(vec.X / scalar, vec.Y / scalar, vec.Z / scalar)
+
+def rotate_vector(vector: WrappedStruct, angle_degrees: float) -> WrappedStruct:
+    angle_radians = radians(angle_degrees)
+    x = vector.X * cos(angle_radians) - vector.Y * sin(angle_radians)
+    y = vector.X * sin(angle_radians) + vector.Y * cos(angle_radians)
+    return get_vector(x, y, vector.Z)
+
+def get_normalized_vector(rotation: WrappedStruct) -> WrappedStruct:
+    yaw_rad = radians(rotation.Yaw)
+    pitch_rad = radians(rotation.Pitch)
+
+    x = cos(yaw_rad) * cos(pitch_rad)
+    y = sin(yaw_rad) * cos(pitch_rad)
+    z = sin(pitch_rad)
+
+    magnitude = (x**2 + y**2 + z**2)**0.5
+    if magnitude == 0:
+        return get_vector(0, 0, 0)
+    return get_vector(x / magnitude, y / magnitude, z / magnitude)
+
+def get_valid_loot(items: List[UObject]) -> List[UObject]:
+    valid_loot = []
+
+    for loot in items:
+        class_name = loot.Class.Name
+        if not include_echo_logs.value and "EchoLogPickup" in class_name:
+            continue
+        if loot.Class == unrealsdk.find_class("OakMissionPickup") or loot.Class == unrealsdk.find_class("GunRackDisplayItem"):
+            continue
+        valid_loot.append(loot)
+
+    return valid_loot
+
+def try_get_item_real_category(item: UObject) -> str:
+    try:
+        balance_component = item.GetInventoryBalanceStateComponent()
+        inv_data = balance_component.InventoryData
+        return inv_data.ItemCardTypeFrameName
+    except:
+        return "Unknown"
+
+class PositionedItem:
+    def __init__(self, item: UObject, position: WrappedStruct | None = None):
+        self.item = item
+        self.position = position or item.K2_GetActorLocation()
+
+    def teleport(self) -> None:
+        self.item.K2_TeleportTo(self.position, self.item.K2_GetActorRotation())
+
+class GroupedLoot:
+    def __init__(self, items: List[PositionedItem]):
+        self.items = items
+
+    def __iter__(self):
+        return iter(self.items)
+
+    def sort(self, player_position: WrappedStruct, normalized_vector: WrappedStruct) -> None:
+        player_forward_position = add_vectors(player_position, multiply_vector_by_scalar(normalized_vector, loot_distance.value))
+
+        match loot_sort_type.value:
+            case "Rarity":
+                rarities = {}
+                for positioned_item in self.items:
+                    rarity: int = positioned_item.item.AssociatedInventoryRarityData.RaritySortValue if positioned_item.item.AssociatedInventoryRarityData else 0
+
+                    if rarity not in rarities:
+                        rarities[rarity] = []
+
+                    rarities[rarity].append(positioned_item)
+
+                rarities = dict(sorted(rarities.items(), key=lambda x: x[0]))
+
+                for i, (rarity, item_array) in enumerate(rarities.items()):
+                    for positioned_item in item_array:
+                        item_position = add_vectors(player_position, multiply_vector_by_scalar(normalized_vector, i * loot_distance.value))
+                        if lay_items_flat.value:
+                            item_position.Z = player_position.Z
+                        positioned_item.position = item_position
+
+            case "Rarity then Type":
+                rarities = {}
+                category_names = set()
+
+                for positioned_item in self.items:
+                    rarity: int = positioned_item.item.AssociatedInventoryRarityData.RaritySortValue if positioned_item.item.AssociatedInventoryRarityData else 0
+                    category_name = try_get_item_real_category(positioned_item.item)
+
+                    if rarity not in rarities:
+                        rarities[rarity] = {}
+
+                    if category_name not in category_names:
+                        category_names.add(category_name)
+
+                    if category_name not in rarities[rarity]:
+                        rarities[rarity][category_name] = []
+
+                    rarities[rarity][category_name].append(positioned_item)
+
+                rarities = dict(sorted(rarities.items(), key=lambda x: x[0]))
+
+                for i, (rarity, rarity_dict) in enumerate(rarities.items()):
+                    rarity_position = add_vectors(player_forward_position, multiply_vector_by_scalar(normalized_vector, i * loot_distance.value))
+                    perpendicular_vector = rotate_vector(normalized_vector, 90)
+                    for j, (category_name, category_array) in enumerate(rarity_dict.items()):
+                        category_offset = multiply_vector_by_scalar(perpendicular_vector, j * loot_distance.value)
+
+                        for positioned_item in category_array:
+                            item_position = add_vectors(rarity_position, category_offset)
+                            if lay_items_flat.value:
+                                item_position.Z = player_position.Z
+                            positioned_item.position = item_position
+
+
+    def teleport_loot(self) -> None:
+        for i, loot in enumerate(self.items):
+            loot.teleport()
 
 @keybind("Teleport Loot")
 def teleport_loot() -> None:
@@ -51,51 +191,23 @@ def teleport_loot() -> None:
     if pc is None:
         return
 
-    pawn = pc.Pawn
-    player_state = pawn.PlayerState
+    player_state = pc.Pawn.PlayerState
     location = player_state.PlayerLocation
     rotation = player_state.PlayerRotation
 
-    px, py, pz = location.X, location.Y, location.Z
+    player_location = get_vector(location.X, location.Y, location.Z)
 
-    x, y, z = rot_to_vec3d(
-        [
-            rotation.Pitch,
-            rotation.Yaw,
-            rotation.Roll,
-        ]
-    )
-    x, y, z = normalize_vec([x, y, 0])
-
-    valid_loot = []
+    normalized_vector = get_normalized_vector(rotation)
 
     all_items = get_current_world().GameState.PickupList
+    valid_loot = get_valid_loot(all_items)
 
-    for loot in all_items:
-        class_name = loot.Class.Name
-        if not include_echo_logs.value and "EchoLogPickup" in class_name:
-            continue
-        if loot.Class == unrealsdk.find_class("OakMissionPickup") or loot.Class == unrealsdk.find_class("GunRackDisplayItem"):
-            continue
-        valid_loot.append(loot)
+    grouped_loot = GroupedLoot([PositionedItem(loot) for loot in valid_loot])
+    grouped_loot.sort(player_location, normalized_vector)
+    grouped_loot.teleport_loot()
 
-    loot_by_rarity = {}
-
-    for loot in valid_loot:
-        rarity = 0
-
-        rarity_component = loot.AssociatedInventoryRarityData
-        if rarity_component is not None:
-            rarity = rarity_component.RaritySortValue
-
-        if rarity not in loot_by_rarity:
-            loot_by_rarity[rarity] = []
-
-        loot_by_rarity[rarity].append(loot)
-
-    for i, loot in enumerate(loot_by_rarity.values()):
-        for loot_pickup in loot:
-            loot_pickup.K2_TeleportTo(get_vector(px + 75 * x * i, py + 75 * y * i, pz), loot_pickup.K2_GetActorRotation())
-            z += 30
-
-build_mod(options=[include_echo_logs])
+build_mod(
+    options=[
+        loot_distance, loot_sort_type, include_echo_logs, lay_items_flat
+    ]
+)
